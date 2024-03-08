e363cff837286839ac51a9be770cb1bf38439cc4
ProblemaTheu <matheus26.fernandes@gmail.com>
2024-03-08 12:47:01 -0300

    more adjusts

--- script/contact-form-process.php
+++ script/contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "";
+    $email_to = "COLOCAR O EMAIL DE MAINHA AQUI";
     $email_subject = "New form submissions";
 
     function problem($error)
